214ada4e34fe37639e14af036bc8d1ded12f3839
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-11-21 14:24:59 +0100

    1 file modified

--- src/app/select-custom-trigger-example.ts
+++ src/app/select-custom-trigger-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 export interface MatOptionsGroup {
   label: string;
@@ -13,19 +13,17 @@ export interface MatOptionsGroup {
   styleUrls: ['select-custom-trigger-example.css'],
 })
 export class SelectCustomTriggerExample {
-  toppings = new FormControl();
   isExpandCategory: boolean[] = [];
   states = new FormControl();
 
   expandDocumentTypes(group: any) {
-    console.log("expanding dropdown", group);
+    console.log('expanding dropdown', group);
     this.isExpandCategory[group.label] = !this.isExpandCategory[group.label];
     // expand only selected parent dropdown category with that childs
   }
 
-  
   optionClicked(name: string, index?: number) {
-    console.log("Selected option: ", name, 'having id: ', index);
+    console.log('Selected option: ', name, 'having id: ', index);
   }
 
   toggleSelection(event: any, group: any) {
@@ -33,79 +31,114 @@ export class SelectCustomTriggerExample {
     //console.log(event.checked);
     let states = this.states.value;
     states = states ? states : [];
-    if(event.checked) {
-      states.push(...group.options)
+    if (event.checked) {
+      states.push(...group.options);
     } else {
       group.options.forEach((x: string) => states.splice(states.indexOf(x), 1));
     }
     this.states.setValue(states);
     //console.log(states);
-    console.log(this.states.value);
+    // console.log(this.states.value);
     // here select all childs for this particular group
   }
 
-  optionsGroups: MatOptionsGroup[] = [{
-    label: 'A',
-    options: ['Alabama', 'Alaska', 'Arizona', 'Arkansas']
-  }, {
-    label: 'C',
-    options: ['California', 'Colorado', 'Connecticut']
-  }, {
-    label: 'D',
-    options: ['Delaware']
-  }, {
-    label: 'F',
-    options: ['Florida']
-  }, {
-    label: 'G',
-    options: ['Georgia']
-  }, {
-    label: 'H',
-    options: ['Hawaii']
-  }, {
-    label: 'I',
-    options: ['Idaho', 'Illinois', 'Indiana', 'Iowa']
-  }, {
-    label: 'K',
-    options: ['Kansas', 'Kentucky']
-  }, {
-    label: 'L',
-    options: ['Louisiana']
-  }, {
-    label: 'M',
-    options: ['Maine', 'Maryland', 'Massachusetts', 'Michigan',
-      'Minnesota', 'Mississippi', 'Missouri', 'Montana']
-  }, {
-    label: 'N',
-    options: ['Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-      'New Mexico', 'New York', 'North Carolina', 'North Dakota']
-  }, {
-    label: 'O',
-    options: ['Ohio', 'Oklahoma', 'Oregon']
-  }, {
-    label: 'P',
-    options: ['Pennsylvania']
-  }, {
-    label: 'R',
-    options: ['Rhode Island']
-  }, {
-    label: 'S',
-    options: ['South Carolina', 'South Dakota']
-  }, {
-    label: 'T',
-    options: ['Tennessee', 'Texas']
-  }, {
-    label: 'U',
-    options: ['Utah']
-  }, {
-    label: 'V',
-    options: ['Vermont', 'Virginia']
-  }, {
-    label: 'W',
-    options: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-  }];
+  optionsGroups: MatOptionsGroup[] = [
+    {
+      label: 'A',
+      options: ['Alabama', 'Alaska', 'Arizona', 'Arkansas'],
+    },
+    {
+      label: 'C',
+      options: ['California', 'Colorado', 'Connecticut'],
+    },
+    {
+      label: 'D',
+      options: ['Delaware'],
+    },
+    {
+      label: 'F',
+      options: ['Florida'],
+    },
+    {
+      label: 'G',
+      options: ['Georgia'],
+    },
+    {
+      label: 'H',
+      options: ['Hawaii'],
+    },
+    {
+      label: 'I',
+      options: ['Idaho', 'Illinois', 'Indiana', 'Iowa'],
+    },
+    {
+      label: 'K',
+      options: ['Kansas', 'Kentucky'],
+    },
+    {
+      label: 'L',
+      options: ['Louisiana'],
+    },
+    {
+      label: 'M',
+      options: [
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+      ],
+    },
+    {
+      label: 'N',
+      options: [
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+      ],
+    },
+    {
+      label: 'O',
+      options: ['Ohio', 'Oklahoma', 'Oregon'],
+    },
+    {
+      label: 'P',
+      options: ['Pennsylvania'],
+    },
+    {
+      label: 'R',
+      options: ['Rhode Island'],
+    },
+    {
+      label: 'S',
+      options: ['South Carolina', 'South Dakota'],
+    },
+    {
+      label: 'T',
+      options: ['Tennessee', 'Texas'],
+    },
+    {
+      label: 'U',
+      options: ['Utah'],
+    },
+    {
+      label: 'V',
+      options: ['Vermont', 'Virginia'],
+    },
+    {
+      label: 'W',
+      options: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming'],
+    },
+  ];
 }
-
 
 /**  Copyright 2020 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
